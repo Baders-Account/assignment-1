321b7fcc00dca6b16ed00a1af6470d9f118a0465
Just-Learning-here <108979880+Just-Learning-here@users.noreply.github.com>
2025-09-25 12:36:27 +0300

    added Dark/Light theme toggle

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+function toggleTheme() {
+     
+        var body = document.body; // Get the body element
+        body.classList.toggle('dark-mode'); // Toggle the 'dark-mode' class
+        
+
+   
+}
+
+
+console.log("Script loaded");
